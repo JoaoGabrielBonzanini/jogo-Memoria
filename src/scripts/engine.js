a71ad0a033c3e61f39ea11b55ce80b9972c8a3e8
JoaoGabrielBonzanini <joaogabrielbonzanini@gmail.com>
2023-12-06 11:10:53 -0300

    first steps of the engine and css adjustments

---
+++ src/scripts/engine.js
@@ -0,0 +1,44 @@
+const emojis = [
+"☠️",
+"☠️",
+"💶",
+"💶",
+"🤌",
+"🤌",
+"⚽",
+"⚽",
+"🏀",
+"🏀",
+"🍻",
+"🍻",
+"♨️",
+"♨️",
+"⛈️",
+"⛈️"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5) ? 2:-1); 
+
+for(let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }   
+}
+
+function checkMatch(){
+    
+}
